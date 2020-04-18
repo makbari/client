17f2d74bd94e68e02d56754eb8bf3a9daa489f8b
makbari <mohammad-akbari@hotmail.com>
2020-04-18 21:52:09 +0200

    done

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,8 @@
+import "./styles/index.scss";
 const app: HTMLElement | null = document.getElementById("app");
 if (app) {
-  app.innerHTML = "<h1>Hello Typescript </h1>";
+  app.innerHTML = `<h1>Hello Typescript from ${
+    __DEV__ ? "development" : "production"
+  }</h1>`;
 }
+console.log("environment is in development: ", __DEV__);
